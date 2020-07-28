b028984755845497daf49dde2b7614b9cb7b2c71
Riya-Anadkat <riya.anadkat@gmail.com>
2020-07-28 18:45:15 -0400

    Updates

---
+++ precache-manifest.74456a1525a5d126b916a83045107011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0150fd923b618d17fd760e2aa9fb6364",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "346a0fec7c869b81f436",
+    "url": "/TicTacToe/static/css/main.6db3dddf.chunk.css"
+  },
+  {
+    "revision": "7de1f036a2157dac2058",
+    "url": "/TicTacToe/static/js/2.57343eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.57343eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "346a0fec7c869b81f436",
+    "url": "/TicTacToe/static/js/main.e334a3b1.chunk.js"
+  },
+  {
+    "revision": "ebdbc3f3c36f0e8a7009",
+    "url": "/TicTacToe/static/js/runtime-main.3e2f971d.js"
+  }
+]);
